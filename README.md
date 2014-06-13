05ae2bf1142d71efb25e151fe0efefa3c574f812
deathcap <deathcap@gmx.co.uk>
2014-06-13 00:30:41 -0700

    Add experimental glass cover custom block model
    Requires deathcap/voxel-mesher@9c9cc67995bad2f06f1e4c6de8e0ebed10afc539
    and deathcap/voxel-shader@4472025a6598880ef10cf80637a3304a66c2b214

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 # voxel-glass
 
+Glass blocks (voxel.js plugin)
+
+![screenshot](http://i.imgur.com/cSUuVZP.png "Screenshot")
+
+Blocks added include:
+
+* `glass`: a transparent solid block (full cube)
+* `glassCover`: custom model, 1/16th height, translucent blue (experimental)
+
+The glass cover requires the latest versions of [voxel-mesher](https://github.com/deathcap/voxel-mesher) and [voxel-shader](https://github.com/deathcap/voxel-shader),
+and is an example of using [block-models](https://github.com/deathcap/block-models),
+via the `blockModel` property in [voxel-registry](https://github.com/deathcap/voxel-registry)
+block registration.
+
 ## License
 
 MIT
--- glass.js
+++ glass.js
@@ -13,6 +13,23 @@ function GlassPlugin(game, opts) {
 
 GlassPlugin.prototype.enable = function() {
   this.registry.registerBlock('glass', {texture: 'glass', transparent: true, hardness: 0.2});
+  this.registry.registerBlock('glassCover', { // TODO: all other colors, and orientations
+    displayName: 'Glass Cover',
+    texture: 'glass_blue',
+    blockModel:
+      [{from: [0,0,0],
+      to: [16,1,16], // 1/16th flat above ground
+      faceData: {
+        down: {},
+        up: {},
+        north: {},
+        south: {},
+        west: {},
+        east: {}
+        },
+      texture: 'glass_blue',
+      }],
+  });
 };
 
 GlassPlugin.prototype.disable = function() {
