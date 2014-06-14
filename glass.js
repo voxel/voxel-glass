4d181ca292c0433da75454455d75a9616a967f8c
deathcap <deathcap@gmx.co.uk>
2014-06-14 16:57:05 -0700

    Place X or Z panes depending on player orientation. Closes GH-1
    This way there is only one item as far the player is concerned, but when
    placed in the world it can face along the X or Z axis.

--- glass.js
+++ glass.js
@@ -1,20 +1,60 @@
 'use strict';
 
+var ItemPile = require('itempile');
+
 module.exports = function(game, opts) {
   return new GlassPlugin(game, opts);
+};
+module.exports.pluginInfo = {
+  loadAfter: ['voxel-registry', 'voxel-use']
 };
 
 function GlassPlugin(game, opts) {
   this.registry = game.plugins.get('voxel-registry');
   if (!this.registry) throw new Error('voxel-glass requires voxel-registry');
 
+  this.use = game.plugins.get('voxel-use');
+  if (!this.use) throw new Error('voxel-glass requires voxel-use');
+
   this.enable();
 }
 
+// Get X or Z depending on the player orientation
+GlassPlugin.prototype.playerOrientation = function() {
+  var heading = Math.atan2(self.game.cameraVector()[0], self.game.cameraVector()[2]);
+  var dir;
+  if (Math.abs(heading) <= Math.PI / 4) { // 0 +/- 45 degrees // TODO: refactor with voxel-pumpkin, generic block/player orientation module?
+    return 'Z'; // north
+  } else if (Math.PI - Math.abs(heading) <= Math.PI / 4) { // +/-180 +/- 45
+    return 'Z'; // south
+  } else if (heading > 0) { // +90 +/- 45
+    return 'X'; // west
+  } else { // if (heading <= 0) { // -90 +/- 45
+    return 'X'; // east
+  }
+};
+
 GlassPlugin.prototype.enable = function() {
   this.registry.registerBlock('glass', {texture: 'glass', transparent: true, hardness: 0.2});
-  this.registry.registerBlock('glassPaneZ', {
+
+  // item
+  var self = this;
+  this.registry.registerItem('glassPane', {
     displayName: 'Glass Pane',
+    itemTexture: 'glass_blue',
+    creativeTab: 'blocks', // TODO: another decorative tab? glass tab?
+    onUse: function(held, target) {
+      // place X or Z pane depending on facing
+      return self.use.useBlock(target, new ItemPile('glassPane' + self.playerOrientation())) === undefined;
+    },
+  });
+
+  // oriented blocks
+
+  this.registry.registerBlock('glassPaneZ', {
+    creativeTab: false,
+    itemDrop: 'glassPane',
+    displayName: 'Glass Pane Z',
     itemTexture: 'glass_blue',  // flat, not 3D cube
     texture: 'glass_blue',      // preload for model below
     blockModel:
@@ -34,7 +74,9 @@ GlassPlugin.prototype.enable = function() {
 
   // same as above but oriented along X axis
   this.registry.registerBlock('glassPaneX', {
-    displayName: 'Glass Pane',
+    creativeTab: false,
+    itemDrop: 'glassPane',
+    displayName: 'Glass Pane X',
     itemTexture: 'glass_blue',
     texture: 'glass_blue',
     blockModel:
